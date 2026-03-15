9c9873a095cc9ae5111b01d71d7be489874a829f
Mobility-Marine <a.reyes@mobility-marine.com>
2026-03-15 15:14:03 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ if (!companyId) {
         gridTemplateColumns: "280px 1fr",
       }}
     >
-      {/* SIDEBAR */}
+      {/* SIDEBAR O */}
       <aside
         style={{
           background: "#0b1733",
