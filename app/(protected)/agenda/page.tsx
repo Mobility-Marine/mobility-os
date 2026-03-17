55eca091a32b64c83a67ca1bdb398c1b5734b193
Mobility-Marine <a.reyes@mobility-marine.com>
2026-03-16 23:14:01 -0600

    Create page.tsx

---
+++ app/(protected)/agenda/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Agenda from "./components/Agenda";
+
+export default function AgendaPage() {
+  return (
+    <div>
+      <h1 style={{ marginBottom: 16 }}>Agenda</h1>
+      <Agenda />
+    </div>
+  );
+}
