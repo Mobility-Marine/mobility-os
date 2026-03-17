18461918e71b79d8f8a28e2ccf1eddbda0794216
Mobility-Marine <a.reyes@mobility-marine.com>
2026-03-17 00:39:49 -0600

    Create page.tsx

---
+++ app/(protected)/comercial/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function AreaPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 28, fontWeight: 700 }}>
+        Área en construcción
+      </h1>
+
+      <p style={{ marginTop: 8, color: "#9fb3d9" }}>
+        Este módulo estará disponible próximamente.
+      </p>
+    </div>
+  );
+}
