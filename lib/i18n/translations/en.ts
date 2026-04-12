3bf51baf9e9fd9455d4277a9b2209dfa2297ee03
Mobility-Marine <a.reyes@mobility-marine.com>
2026-04-12 16:40:25 -0600

    Update en.ts

--- lib/i18n/translations/en.ts
+++ lib/i18n/translations/en.ts
@@ -24,7 +24,7 @@ export const en: TranslationKeys = {
     optional:   "Optional",
     required:   "Required",
     none:       "No recurrence",
-    reset: "Restablecer",
+    reset:      "Reset",
   },
 
   nav: {
