e5a1053df075f2142a332f694eb2eed1b8086f36
Mobility-Marine <a.reyes@mobility-marine.com>
2026-04-14 17:29:54 -0600

    Create page.tsx

---
+++ app/(protected)/finanzas/flujo-efectivo/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Page() {
+  return (
+    <div style={{ padding: "32px" }}>
+      <h1 style={{ fontSize: 24, fontWeight: 700 }}>Módulo</h1>
+      <p style={{ marginTop: 8, color: "#9fb3d9" }}>En construcción.</p>
+    </div>
+  );
+}
