6de9d93f74c92c79a755c01d5e88e3a9b781c360
Mobility-Marine <a.reyes@mobility-marine.com>
2026-04-16 21:05:55 -0600

    Delete app/(protected)/company/page.tsx

--- app/(protected)/company/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-export default function CompanyPage() {
-  const router = useRouter();
-  useEffect(() => { router.replace("/settings"); }, []);
-  return null;
-}
